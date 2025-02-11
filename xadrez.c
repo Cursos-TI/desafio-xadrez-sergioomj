cf1a3a274e1bbac8ed169ba81b8d052bc7338bc4
sergioomj <sergiomoreirajr88@gmail.com>
2025-02-11 20:00:16 -0300

    att xadrez com cavalo.c

--- xadrez.c
+++ xadrez.c
@@ -30,24 +30,46 @@ void moverBispo(){
             printf(ESQUERDA);
         }
     }
+    // mover cavalo em formato L (baixo / esquerda)
+    void moverCavalo(){
+        printf("Mover cavalo em L (Baixo / Esquerda: \n");
+        // Primeira parte da movimentação em L : 2 passos para baixo
+        for(int i = 1; i <= 2; i++){
+            printf(BAIXO);
+        }
+        // Segunda parte do movimento em L: 1 paso para esquerda
+        int passo = 1;
+        while(passo <= 1){
+            printf(ESQUERDA);
+            passo++;
+    }
+}
+        
+        
 
     int main(){
         //variáveis de controle de peças
         int bispo_movimento = 1;
         int torre_movimento = 1;
         int rainha_movimento = 1;
-
+        int cavalo_movimento = 1;
+        // movimento bispo
         if(bispo_movimento){
             moverBispo();
         }
-
+        //movimento torre
         if(torre_movimento){
             moverTorre();
         }
-
+        // movimento rainha
         if(rainha_movimento){
             moverRainha();
         }
+        //movimento cavalo
+        if(cavalo_movimento){
+            moverCavalo();
+        }
+        
 
         return 0;
     }
