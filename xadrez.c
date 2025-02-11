1680bc580c20c9f13efa204c29b833ffdd3b8868
sergioomj <sergiomoreirajr88@gmail.com>
2025-02-11 19:39:17 -0300

    att xadrez.c

--- xadrez.c
+++ xadrez.c
@@ -1,32 +1,53 @@
 #include <stdio.h>
 
-// Desafio de Xadrez - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
-// O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
+//Constantes para as direções
+#define CIMA "Cima\n"
+#define BAIXO "Baixo\n"
+#define ESQUERDA "Esquerda\n"
+#define DIREITA "Direita\n"
 
-int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
-
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
-
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
-
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
-
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
-
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
-
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
-
-    return 0;
+//Mover o Bispo cima/direita
+void moverBispo(){
+    printf("Movendo Bispo 5 casas na diagonal cima/ direita: \n");
+    for(int i = 1; i <= 5; i++){
+        printf(CIMA);
+        printf(DIREITA);
+    }
 }
+
+    //Mover a Torre para direita
+    void moverTorre(){
+        printf("Mover torre 5 casas para direita: \n");
+        for( int i = 1; i <= 5; i++){
+            printf(DIREITA);
+        }
+    }
+
+    //Mover Rainha para a Esquerda
+    void moverRainha(){
+        printf("Mover rainha 8 casas para a esquerda: \n");
+        for(int i = 1; i <= 8; i++){
+            printf(ESQUERDA);
+        }
+    }
+
+    int main(){
+        //variáveis de controle de peças
+        int bispo_movimento = 1;
+        int torre_movimento = 1;
+        int rainha_movimento = 1;
+
+        if(bispo_movimento){
+            moverBispo();
+        }
+
+        if(torre_movimento){
+            moverTorre();
+        }
+
+        if(rainha_movimento){
+            moverRainha();
+        }
+
+        return 0;
+    }
